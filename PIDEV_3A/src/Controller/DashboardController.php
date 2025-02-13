c7b396ab7e9ebe11bdd3ee7b274815480710c5ee
FawziSaidi <fawzi.saidijr@gmail.com>
2025-02-13 08:45:59 +0100

    Hotfix : Feature of administration for users : the C

--- PIDEV_3A/src/Controller/DashboardController.php
+++ PIDEV_3A/src/Controller/DashboardController.php
@@ -15,6 +15,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
 
 final class DashboardController extends AbstractController
 {
@@ -45,69 +50,85 @@ final class DashboardController extends AbstractController
 
 
     #[Route('/dashboard/users', name: 'app_users_table')]
-    public function usersTable(UserRepository $userRepository): Response
-    {
-        $registrationForm = $this->createForm(RegistrationFormType::class);
-        $users = $userRepository->findAll();
-
-        return $this->render('dashboard/users_table.html.twig', [
-            'users' => $users,
-            'registrationForm' => $registrationForm->createView(),
-        ]);
-    }
-
-    #[Route('/dashboard/users/add', name: 'app_user_add', methods: ['POST'])]
-    public function addUser(Request $request): Response
+    public function usersTable(UserRepository $userRepository, Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(RegistrationFormType::class);
         $form->handleRequest($request);
-
+    
         if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-            $role = $data['role'];
-
+            $role = $form->get('role')->getData();
+    
             switch ($role) {
                 case 'ROLE_STUDENT':
                     $user = new Student();
-                    $user->setDateOfBirth($data['date_of_birth']);
-                    $user->setYearOfStudy($data['year_of_study']);
-                    $user->setCoursesEnrolled($data['courses_enrolled']);
-                    $user->setCertifications($data['certifications']);
-                    $user->setDiplomas($data['diplomas']);
+                    $user->setDateOfBirth($form->get('date_of_birth')->getData());
+                    $user->setYearOfStudy($form->get('year_of_study')->getData());
+                    $user->setCoursesEnrolled($form->get('courses_enrolled')->getData());
+                    $user->setCertifications($form->get('certifications')->getData());
+                    $user->setDiplomas($form->get('diplomas')->getData());
                     break;
                 case 'ROLE_TEACHER':
                     $user = new Teacher();
-                    $user->setSpecialization($data['specialization']);
-                    $user->setYearsOfExperience($data['years_of_experience']);
+                    $user->setSpecialization($form->get('specialization')->getData());
+                    $user->setYearsOfExperience($form->get('years_of_experience')->getData());
                     break;
                 case 'ROLE_HRM_CLUB':
                     $user = new HrmClub();
-                    $user->setEvent($data['event']);
+                    $user->setEvent($form->get('event')->getData());
                     break;
                 case 'ROLE_HRM_STAGE':
                     $user = new HrmStage();
-                    $user->setCompany($data['company']);
+                    $user->setCompany($form->get('company')->getData());
                     break;
                 default:
                     $user = new User();
             }
-
-            // Set common fields
-            $user->setEmail($data['email']);
-            $user->setPassword($this->passwordHasher->hashPassword($user, $data['plainPassword']));
-            $user->setFirstName($data['first_name']);
-            $user->setLastName($data['last_name']);
-            $user->setBio($data['bio']);
-            $user->setPhoneNumber($data['phone_number']);
-            $user->setAdress($data['adress']);
-            $user->setAvatar($data['avatar']);
+    
+            $user->setEmail($form->get('email')->getData());
+            $user->setFirstName($form->get('first_name')->getData());
+            $user->setLastName($form->get('last_name')->getData());
             $user->setRoles([$role]);
-
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
-
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $user->setCreatedAt(new \DateTimeImmutable());
+            $user->setPhoneNumber($form->get('phone_number')->getData());
+            $user->setBio($form->get('bio')->getData());
+            $user->setAdress($form->get('adress')->getData());
+    
+            $avatarFile = $form->get('avatar')->getData();
+            if ($avatarFile) {
+                $originalFilename = pathinfo($avatarFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$avatarFile->guessExtension();
+    
+                try {
+                    $avatarFile->move(
+                        $this->getParameter('avatars_directory'),
+                        $newFilename
+                    );
+                    $user->setAvatar($newFilename);
+                } catch (FileException $e) {
+                    // Handle file upload error
+                }
+            }
+    
+            $entityManager->persist($user);
+            $entityManager->flush();
+    
             $this->addFlash('success', 'User added successfully.');
+    
             return $this->redirectToRoute('app_users_table');
         }
-    }  
+    
+        $users = $userRepository->findAll();
+    
+        return $this->render('dashboard/users_table.html.twig', [
+            'users' => $users,
+            'registrationForm' => $form->createView(),
+        ]);
+    }
 }
